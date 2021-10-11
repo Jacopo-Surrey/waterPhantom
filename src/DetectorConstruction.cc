22b0fa7dc97687a9022753ae333b941ba8cf76bd
Jacopo-Surrey <j.magini@surrey.ac.uk>
2021-10-11 16:15:25 +0100

    Use water from NIST instead of building it -- correct ionisation potential

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -81,30 +81,33 @@ DetectorConstruction::~DetectorConstruction(){
 
 G4VPhysicalVolume* DetectorConstruction::Construct()
 {
-	
-	//Define Hydrogen 
-	G4double A = 1.01 * g/mole;
-	G4double Z = 1;
-	G4Element* elH = new G4Element ("Hydrogen", "H", Z, A);
-	
-	//Define Oxygen
-	A = 16.0 * g/mole;
-	Z = 8;
-	G4Element* elO = new G4Element ("Oxygen", "O", Z, A);
-	
 	//Define Vacuum
-	A = 1.01*g/mole;
-	Z = 1.;
+	G4double A = 1.01*g/mole;
+	G4double Z = 1.;
 	G4double vacuumDensity = 1.e-25 *g/cm3;
 	G4double pressure = 3.e-18*pascal;
 	G4double temperature = 2.73*kelvin;
 	G4Material* vacuum = new G4Material("Galactic", Z, A,
 			         vacuumDensity,kStateGas,temperature,pressure);
 
+	/*
+	//Define Hydrogen 
+	A = 1.01 * g/mole;
+	Z = 1;
+	G4Element* elH = new G4Element ("Hydrogen", "H", Z, A);
+	
+	//Define Oxygen
+	A = 16.0 * g/mole;
+	Z = 8;
+	G4Element* elO = new G4Element ("Oxygen", "O", Z, A);
+
 	//define water
 	G4Material* water = new G4Material("water", 1*g/cm3, 2);
 	water -> AddElement(elH, 2);
 	water -> AddElement(elO, 1);
+	*/
+	
+	G4Material* water = G4NistManager::Instance()->FindOrBuildMaterial("G4_WATER");
 	
 	//Define volumes
 	// World volume  has size 1cm
